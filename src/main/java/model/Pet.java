5279a84c48c0cca1ca8b4a1678405f357eca6975
NsovoRisima <68904559+NsovoRisima@users.noreply.github.com>
2020-07-29 07:28:25 +0200

    Add files via upload

---
+++ src/main/java/model/Pet.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.util.List;
+
+public class Pet {
+
+    long id;
+    String name;
+    Category category;
+    List<String> photoUrls;
+    List<Category>tags;
+    String status;
+
+    public Pet() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public List<Category> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Category> tags) {
+        this.tags = tags;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
---
+++ src/test/resources/features/status/check_on_application_status.feature
@@ -0,0 +1,5 @@
+#Feature: Check on application status
+#  Scenario: Application status end-point
+#    Given the application is running
+#    When I check the application status
+#    Then the API should return "Serenity REST Starter project up and running"
